d681d31de9451e6d36181de4344f5bb353e12537
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-09-18 13:47:36 +0200

    add pomodoro

--- sloth-selfie/src/App.js
+++ sloth-selfie/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import Calendar from 'react-calendar';
@@ -24,46 +23,62 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sloth Selfie</h1>
-        <Carousel showThumbs={false} showStatus={false} infiniteLoop={true}>
-          <div class="carousel-slide">
-            <h2>Calendar</h2>
-            <Calendar />
-          </div>
-          <div class="carousel-slide">
-          <h2>Notes</h2>
-            <div className="notes-section">
-              <input
-                value={noteTitle}
-                onChange={(e) => setNoteTitle(e.target.value)}
-                placeholder="Note Title"
-              />
-              <textarea
-                value={noteContent}
-                onChange={(e) => setNoteContent(e.target.value)}
-                placeholder="Note Content"
-              />
-              <button onClick={handleAddNote}>Add Note</button>
-              <ul>
-                {notes.map((note, index) => (
-                   <li key={index}>
-                   <h3>{note.title}</h3>
-                   <p>{note.content}</p>
-                 </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div class="carousel-slide">
-            <h2>Other Stuff 2</h2>
-            <p>Content for other stuff 2</p>
-          </div>
-        </Carousel>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Sloth Selfie</h1>
+          <Routes>
+            <Route path="/" 
+            element={
+              <Carousel showThumbs={false} showStatus={false} infiniteLoop={true}>
+                <div class="carousel-slide">
+                  <h2>Calendar</h2>
+                  <Calendar />
+                </div>
+                <div class="carousel-slide">
+                <h2>Notes</h2>
+                  <div className="notes-section">
+                    <input
+                      value={noteTitle}
+                      onChange={(e) => setNoteTitle(e.target.value)}
+                      placeholder="Note Title"
+                    />
+                    <textarea
+                      value={noteContent}
+                      onChange={(e) => setNoteContent(e.target.value)}
+                      placeholder="Note Content"
+                    />
+                    <button onClick={handleAddNote}>Add Note</button>
+                    <ul>
+                      {notes.map((note, index) => (
+                        <li key={index}>
+                        <h3>{note.title}</h3>
+                        <p>{note.content}</p>
+                      </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div className="carousel-slide">
+                    {/* Preview of the Pomodoro timer */}
+                    <h2>Pomodoro</h2>
+                    <div className="pomodoro-timer">
+                      <img src={animatedHourglass} alt="Hourglass" className="hourglass"/><br/>
+                      <p>Start your study session!</p><br/>
+                      <Link to="/pomodoro">
+                        <button>Start</button>
+                      </Link>
+                    </div>
+                  </div>
+              </Carousel>
+            }
+            />
+            <Route path="/pomodoro" element={<PomodoroFunction />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
+  ); 
 }
 
 export default App;
