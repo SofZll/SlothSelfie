2a66063ace1797289cd9cf9b7a65bbedb4f411be
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-09-20 17:13:03 +0200

    carousel

--- sloth-selfie/src/App.js
+++ sloth-selfie/src/App.js
@@ -1,16 +1,14 @@
 import iconHome from './media/Sloth.svg';
 import React, {useState} from 'react';
 import './css/App.css';
-import { BrowserRouter as Router, Route, Routes, Link , Navigate} from 'react-router-dom';
-import { Carousel } from 'react-responsive-carousel';
-import Calendar from 'react-calendar';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import 'react-calendar/dist/Calendar.css';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import EventsFunction from './Events';
 import PomodoroFunction from './Pomodoro';
 import NotesFunction from './Notes';
-import animatedHourglass from './media/Hourglass.gif';
 import Login from './Login';
+import Card from "./cardCarosel";
+import Carousel from "./CarouselHome";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
 /*
@@ -20,12 +18,42 @@ function App() {
     setIsAuthenticated(status);
   };
 */
+  let cards = [
+    {
+      key: uuidv4(),
+      content: (
+        <Card title="Calendar" caseShow="1"/>
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card title="Notes" caseShow="2"/>
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card title="Pomodoro" caseShow="3"/>
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card title="Projects" caseShow="4"/>
+      )
+    }
+  ];
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Sloth Selfie</h1>
-        <img src={iconHome} className="App-logo" alt="logo" />
+          <div className="title">
+            <h1>Sloth Selfie</h1>
+            <img src={iconHome} className="App-logo" alt="logo" />
+          </div>
+          
         <Routes>
             {/* <Route path="/login" element={<Login onLogin={handleLogin} />} /> */}
             {/* Commenta il controllo di autenticazione */}
@@ -42,42 +70,14 @@ function App() {
             <Route path="/"  /*path="/home"*/
               element={ 
               /*isAuthenticated ? (*/ 
-                <Carousel showThumbs={false} showStatus={false} infiniteLoop={true}>
-                  <div class="carousel-slide">
-                    <h2>Calendar</h2>
-                    <Calendar />
-                    <Link to="/events">
-                      <button className="btn">Manage Events</button>
-                    </Link>
-                    <Link to="/activities">
-                      <button className="btn">Manage Activities</button>
-                    </Link>
-                  </div>
-                  <div class="carousel-slide">
-                    <h2>Notes</h2>
-                      <div className="notes-section">
-                      <p>Add a note here!</p><br/>
-                      <Link to="/notes">
-                        <button className="btn">Add</button>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="carousel-slide">
-                    {/* Preview of the Pomodoro timer */}
-                    <h2>Pomodoro</h2>
-                    <div className="pomodoro-timer">
-                      <img src={animatedHourglass} alt="Hourglass" className="hourglass"/><br/>
-                      <p>Start your study session!</p><br/>
-                      <Link to="/pomodoro">
-                        <button className="btn">Start</button>
-                      </Link>
-                    </div>
-                  </div>
-                <div class="carousel-slide">
-                  <h2>Projects</h2>
-                  <p>Content for other stuff 3</p>
-                </div>
-                </Carousel>
+              <Carousel
+                cards={cards}
+                height="700px"
+                width="800px"
+                margin="0 auto"
+                offset={2}
+                showArrows={false}
+              />
               /*) : (
                 <Navigate to="/login" />
               )*/
