e750e8d6280845e8147b6c8d965fd440a37df7ed
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-09-24 15:08:13 +0200

    add btn

--- sloth-selfie/src/CarouselHome.jsx
+++ sloth-selfie/src/CarouselHome.jsx
@@ -1,6 +1,10 @@
 import Carousel from "react-spring-3d-carousel";
 import { useState, useEffect } from "react";
 import { config } from "react-spring";
+import iconCalendar from './media/calendar.svg';
+import iconNotes from './media/notes.svg';
+import iconTomato from './media/tomato.svg';
+import iconProjects from './media/projects.svg'; 
 
 export default function CarroussSel(props) {
   const table = props.cards.map((element, index) => {
@@ -17,19 +21,51 @@ export default function CarroussSel(props) {
     setShowArrows(props.showArrows);
   }, [props.offset, props.showArrows]);
 
+  const handlePrev = () => {
+    setGoToSlide((prev) => (prev > 0 ? prev - 1 : cards.length - 1));
+  };
+
+  const handleNext = () => {
+    setGoToSlide((prev) => (prev < cards.length - 1 ? prev + 1 : 0));
+  };
+
+  const handleGoToSlide = (index) => {
+    setGoToSlide(index);
+  };
 
   return (
     <div className="carousel"
       style={{ width: props.width, height: props.height, margin: props.margin }}
     >
-      
+      <div className="divBtn">
+        <button onClick={handlePrev} className="btn">
+          ⬅️
+        </button>
+        <button onClick={handleNext} className="btn">
+          ➡️
+        </button>
+      </div>
       <Carousel
         slides={cards}
         goToSlide={goToSlide}
         offsetRadius={offsetRadius}
         showNavigation={showArrows}
         animationConfig={config.gentle}
-        />
+      />
+      <div className="divBtn">
+          <button onClick={() => handleGoToSlide(0)} className="btn">
+            <img src={iconCalendar} alt="icon" className="icon"/>
+          </button>
+          <button onClick={() => handleGoToSlide(1)} className="btn">
+            <img src={iconNotes} alt="icon" className="icon"/>
+          </button>
+          <button onClick={() => handleGoToSlide(2)} className="btn">
+            <img src={iconTomato} alt="icon" className="icon"/>
+          </button>
+          <button onClick={() => handleGoToSlide(3)} className="btn">
+            <img src={iconProjects} alt="icon" className="icon"/>
+          </button>
+      </div>
         
     </div>
     
