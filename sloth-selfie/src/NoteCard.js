2baeac8f6517affcf9b7c483990c1b478755a5e5
giulia-t <giulia.tors@gmail.com>
2024-09-24 10:53:10 +0200

    Note preview

---
+++ sloth-selfie/src/NoteCard.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './css/App.css';
+
+function NoteCard({ note, onDuplicate, onCopy, index, clickedButton }) {
+
+    const [isExpanded, setIsExpanded] = useState(false);  // State to manage the note content expansion
+  
+    const toggleExpand = () => {
+      setIsExpanded(!isExpanded);
+    };
+
+  return (
+    <div className="note-card">
+      <h3>{note.title}</h3>
+      <small>{note.category}</small>
+       {/* Note content: if expanded show all the note text*/}
+       <p onClick={toggleExpand} style={{ cursor: 'pointer' }}>
+        {isExpanded ? note.content : note.content.substring(0, 200) + (note.content.length > 200 ? '...' : '')}
+      </p>
+      <small>{note.date.toLocaleString()}</small>
+      <div className="note-buttons">
+        <button
+          className={`btn btn-duplicate ${clickedButton === 'duplicate' + index ? 'active' : ''}`}
+          onClick={() => onDuplicate(index)}
+        >
+          Duplicate
+        </button>
+        <button
+          className={`btn btn-copy ${clickedButton === 'copy' + index ? 'active' : ''}`}
+          onClick={() => onCopy(note.content)}
+        >
+          Copy
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NoteCard;
