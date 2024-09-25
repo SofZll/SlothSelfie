67cff1ab4ad3f11facdc9ea8c806a74b10f3d61c
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-09-26 00:25:46 +0200

    add transition to all pages

--- sloth-selfie/src/Pomodoro.js
+++ sloth-selfie/src/Pomodoro.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 
 function PomodoroFunction() {
+    // change style page onload document
+    useEffect(() => {
+        const header = document.querySelector('.App-header');
+        const h1 = document.querySelector('h1');
+        if (header) header.classList.add('light-background');
+        else console.error('Header not found');
+        if (h1) h1.classList.add('dark-h1');
+
+        document.body.classList.add('light-background');
+
+        return () => {
+        if (header) header.classList.remove('light-background');
+        if (h1) h1.classList.remove('dark-h1');
+        document.body.classList.remove('light-background');
+        };
+    }, []);
+    
     return (
         <div className="pomodoro">
             <h2>Pomodoro Timer</h2>
