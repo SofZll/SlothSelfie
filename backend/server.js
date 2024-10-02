9225c91aa5c3a4d93b0e0746dbf041d58f47c878
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-10-03 00:13:24 +0200

    user connection

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+global.rootDir = __dirname ;
+global.startDate = null; 
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+
+
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
+
+// Static files from frontend
+const frontendPath = path.join(global.rootDir, '..', 'frontend/build');
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
