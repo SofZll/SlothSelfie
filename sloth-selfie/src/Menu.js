f9ab5e92ae7e34bb42ccd709052a003bacb06868
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-10-16 17:15:55 +0200

    navbar, mobile version to be defined

---
+++ sloth-selfie/src/Menu.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { slide as Hamburger } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import './css/Menu.css';
+
+const Menu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleStateChange = (state) => {
+        setIsOpen(state.isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <Hamburger isOpen={isOpen} onStateChange={handleStateChange}>
+            <Link to="/profile" onClick={closeMenu}>Profile</Link>
+            <Link to="/" onClick={closeMenu}>Home</Link>
+            <Link to="/events" onClick={closeMenu}>Events</Link>
+            <Link to="/activities" onClick={closeMenu}>Activities</Link>
+            <Link to="/notes" onClick={closeMenu}>Notes</Link>
+            <Link to="/pomodoro" onClick={closeMenu}>Pomodoro</Link>
+            {/*<Link to="/projects">Projects</Link>*/}
+        </Hamburger>
+    );
+};
+
+export default Menu;
