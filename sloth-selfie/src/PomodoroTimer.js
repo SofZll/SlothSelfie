7335b617a43dc9bc0532ed7b8c9eb5952df75596
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-10-17 10:23:17 +0200

    finish selection tomato

---
+++ sloth-selfie/src/PomodoroTimer.js
@@ -0,0 +1,16 @@
+
+function PomodoroTimer() {
+    return (
+        <div className="pomodoro-timer">
+            <div className="song-container">
+
+            </div>
+            <div className="pomodoro-container">
+            </div>
+            <div className="stats-container">
+            </div>
+        </div>
+    );
+}
+
+export default PomodoroTimer;
