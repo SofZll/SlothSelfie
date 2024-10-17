218c8ac0bf125cb7d6d4c57b71f515600ecebe91
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-10-17 14:00:35 +0200

    css fix

--- sloth-selfie/src/Profile.js
+++ sloth-selfie/src/Profile.js
@@ -1,34 +1,58 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./css/Profile.css";
-import iconDark from './media/SlothDark.svg';
-import iconLight from './media/SlothLight.svg';
-import { StyleContext } from "./StyleContext";
 
-function Profile({ username, email}) {
-    const { updateStyles, updateIcon } = useContext(StyleContext);
+function Profile({ username }) {
+    const [profileData, setProfileData] = useState({username: '', email: '', profile_image: ''});
 
     useEffect(() => {
-        updateStyles(true);
-        updateIcon(iconDark);
-
-        return () => {
-            updateStyles(false);
-            updateIcon(iconLight);
+        const fetchProfileData = async () => {
+            try {
+                const response = await fetch('api/profile?username=' + username);
+                const data = await response.json();
+                setProfileData(data);
+            } catch (error) {
+                console.error('Error fetching profile data:', error);
+            }
         };
-    }, [updateIcon, updateStyles]);
+
+        fetchProfileData();
+    }, [username]);
+
+    const logout = async () => {
+        try {
+            const response = await fetch('api/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username: username})
+            });
+
+            if (response.ok) {
+                window.location.href = '/login';
+            }
+        } catch (error) {
+            console.error('Error logging out:', error);
+        }   
+    }
 
     return (
         <div className="profile-container">
-            <h2>Profile</h2>
-            <div className="profile-image"></div>
+            <h2 className="profile-title">Profile</h2>
+            <div className="profile-image">
+                {profileData.profile_image_url && (
+                    <img src={profileData.profile_image_url} alt="image" />
+                )}
+            </div>
             <div className="profile-info">
-                <p>Username: {username} </p>
-                <p>Email: {email}</p>
+                <p>Username: {profileData.username} </p>
+                <p>Email: {profileData.email}</p>
             </div>
-            <h2>Pomodoro stats</h2>
+            <h2 className="profile-title">Pomodoro stats</h2>
             <div className="profile-pomodoro-stats">
+                {/* Pomodoro stats content */}
             </div>
-            <button className="btn" onClick="">Log out</button>
+            <button className="btn" onClick={logout}>Log out</button>
         </div>
     );
 }
