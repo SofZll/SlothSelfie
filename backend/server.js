a370b1069d185170eee31d8aa33bd63c5c38f8bb
giulia-t <giulia.tors@gmail.com>
2024-10-27 12:38:39 +0100

    can't find play and pause media, added some comments

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,18 @@ const noteRoutes = require('./routes/routes');
 const path = require('path');
 const connectDB = require('./config/db');
 
+const cors = require('cors');
+
 const app = express();
+
+//locale:
+// Configura CORS per accettare richieste dal frontend locale
+app.use(cors({
+    origin: 'http://localhost:3000', // Origine del tuo frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // se stai inviando cookie o sessioni
+}));
 
 // Connect to MongoDB
 connectDB();
@@ -23,11 +34,15 @@ app.use(session({
     saveUninitialized: true;
 }))
 */
-app.use('/api', Routes);
-//app.use('/api', noteRoutes);
+//app.use('/api', Routes);
+app.use('/api', userRoutes);
+app.use('/api', noteRoutes);
 
 // Static files from frontend
 const frontendPath = path.join(global.rootDir, '..', 'frontend/build');
+//locale:
+//const frontendPath = path.join(global.rootDir, '..', 'sloth-selfie/build');
+
 app.use(express.static(frontendPath));
 
 app.get('*', (req, res) => {
