d548cc2cca73c51a6457486272b4576f739d6880
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-10-31 14:34:22 +0100

    fix edit-image and edit-profile

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { loginUser, registerUser, logoutUser, editImage, editProfile, getUserProfile, getUserUsername} = require('../controllers/userController');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+const { loginUser, registerUser, logoutUser, editImage, editProfile, getUserProfile } = require('../controllers/userController');
 const { fetchState, setTime, resetTime } = require('../controllers/timeMachineController');
 const { createNote, getNotes, updateNote, deleteNote } = require('../controllers/noteController');
 //const { createPost, getPosts } = require('../controllers/hubController');
@@ -19,7 +21,7 @@ const requireAuth = (req, res, next) => {
 router.post('/user/login', loginUser);
 router.post('/user/register', registerUser);
 router.post('/user/logout', logoutUser);
-router.post('/user/edit-image', editImage);
+router.post('/user/edit-image', upload.single('image'), editImage);
 router.post('/user/edit-profile', editProfile);
 router.get('/user/profile', getUserProfile);
 
