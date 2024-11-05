222ac18f3c9e8e5dd69312cc15a49ddf140e2aab
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-11-05 15:40:32 +0100

    small fix

--- sloth-selfie/src/PomodoroAnimation.jsx
+++ sloth-selfie/src/PomodoroAnimation.jsx
@@ -12,33 +12,6 @@ function PomodoroAnimation(isStudioTime, timeLeft, totalTime) {
 
     return (
         <div className="pomodoro-animation">
-            .container
-                .lcd-light
-                .monitor
-                .monitor-neck
-                .monitor-base
-                .desk
-                .tower
-                .chair-back
-                .chair-bottom
-                .clock
-                    .hour
-                    .minute
-                .pillow
-                .bed
-                .human-wrapper
-                    .human
-                    .head
-                        .eyes
-                    .torso
-                        .l-thigh
-                        .l-lower-leg
-                        .r-thigh
-                        .r-lower-leg
-                        .r-upper-arm
-                        .r-lower-arm
-                        .l-upper-arm
-                        .l-lower-arm
         </div>
     );
 }
