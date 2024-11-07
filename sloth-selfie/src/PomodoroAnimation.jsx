eac0b7d31dc5917d67b1d42ea892e997a8f2c9bf
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-11-07 09:24:50 +0100

    add animation

--- sloth-selfie/src/PomodoroAnimation.jsx
+++ sloth-selfie/src/PomodoroAnimation.jsx
@@ -1,17 +1,69 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, CSSProperties} from 'react';
 import './css/pomodoroAnimation.css';
 
-function PomodoroAnimation(isStudioTime, timeLeft, totalTime) {
+function PomodoroAnimation(data) {
 
-    const [percentage, setPercentage] = useState(100);
+    const [penciltime, setPencilTime] = useState('0s');
+    const [linetime, setLineTime] = useState('0s');
+    const [delayGo, setDelayGo] = useState('0s');
+    const [delayBack, setDelayBack] = useState('0s')
+    const [play, setPlay] = useState(false);
+    const [isStudioTime, setIsStudioTime] = useState(true);
 
     useEffect(() => {
-        setPercentage(timeLeft / totalTime * 100);
-    } , [timeLeft, totalTime]);
-    
+        if (data.isStudioTime) {
+            setPencilTime(`${data.studioTime}s`);
+            setLineTime(`${(data.studioTime*0.8)}s`);
+            setDelayGo(`${(data.studioTime*0.2)}s`);
+            setDelayBack(`${(data.studioTime*0.1)}s`);
+        } else {
+            setPencilTime(`${data.breakTime}s`);
+            setLineTime(`${(data.breakTime*0.8)}s`);
+            setDelayGo(`${(data.breakTime*0.2)}s`);
+            setDelayBack(`${(data.breakTime*0.1)}s`);
+        }
+        setPlay(data.isPlaying);
+        setIsStudioTime(data.isStudioTime);
+
+    } , [data]);
+
+
+    useEffect(() => {
+        document.documentElement.style.setProperty('--animation-duration-pencil', penciltime);
+        console.log('penciltime', penciltime);
+        document.documentElement.style.setProperty('--animation-duration-line', linetime);
+        console.log('linetime', linetime);
+        document.documentElement.style.setProperty('--animation-delay-go', delayGo);
+        document.documentElement.style.setProperty('--animation-delay-back', delayBack);
+    } , [penciltime, linetime, delayGo, delayBack]);
+
+
+
+    useEffect(() => {
+        if (play) {
+            document.documentElement.style.setProperty('--animation-running', 'running');
+        } else {
+            document.documentElement.style.setProperty('--animation-running', 'paused');
+        }
+    } , [play]);
+
+
+   
 
     return (
         <div className="pomodoro-animation">
+            <div>
+                <div className={isStudioTime ? 'pencil study-pencil' : 'pencil break-pencil'}>
+                    <span class="lapisContent"></span>
+                    
+                    <span
+                        className={isStudioTime ? 'conteudoStudy conteudo' : 'conteudoBreak conteudo'}>
+                    </span>
+                </div>
+                <span
+                    className={isStudioTime ? 'line lineStudy' : 'line lineBreak'}>
+                </span>
+            </div>
         </div>
     );
 }
