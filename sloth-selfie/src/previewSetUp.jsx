aa675714c708be861124c034e95f0f7776d6903d
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-11-26 00:07:31 +0100

    fix stuff

--- sloth-selfie/src/previewSetUp.jsx
+++ sloth-selfie/src/previewSetUp.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
+import iconCross from './media/crossDark.svg';
+import './css/setting.css';
 
 function PreviewSetUp(props) {
 
+    const handleSetUp = () => {
+        props.setSetUp(!props.setUp);
+    };
+
 
     return (
-        <div>
-        <h1>PreviewSetUp</h1>
+        <div className="settings">
+            <div className="card-settings">
+                <button className="btn-cross" onClick={handleSetUp}>
+                    <img src={iconCross} alt="cross" className="icon-cross" />
+                </button>
+            </div>
         </div>
     );
 }
