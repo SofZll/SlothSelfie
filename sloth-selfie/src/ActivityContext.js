a9ab5aa0eeb43c188691c5c869aa6dd19250c1b2
giulia-t <giulia.tors@gmail.com>
2024-11-27 17:38:43 +0100

    adding  activities currentUser problem

--- sloth-selfie/src/ActivityContext.js
+++ sloth-selfie/src/ActivityContext.js
@@ -1,12 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
 export const ActivityContext = createContext();
 
 export const ActivityProvider = ({ children }) => {
     const [activities, setActivities] = useState([]);
+    const [currUsername, setCurrUsername] = useState('');
 
+    // Get the username of the authenticated user
+    useEffect(() => {
+        const fetchUsername = async () => {
+          try {
+            const response = await fetch('http://localhost:8000/api/user/username', {
+            credentials: 'include'
+            });const data = await response.json();
+            console.log('Username:', data.currUsername);
+            setCurrUsername(data.currUsername);
+        } catch (error) {
+            console.error('Error fetching username:', error);
+        }
+        };
+    
+        fetchUsername();
+    }, []); 
+           
     return (
-        <ActivityContext.Provider value={{ activities, setActivities }}>
+        <ActivityContext.Provider value={{ activities, setActivities, currUsername }}>
             {children}
         </ActivityContext.Provider>
     );
