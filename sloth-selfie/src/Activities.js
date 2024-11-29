7b7b8a3907b2a6d073dd254235cdbdd5700a235c
giulia-t <giulia.tors@gmail.com>
2024-11-29 11:15:39 +0100

    fixed activities+db

--- sloth-selfie/src/Activities.js
+++ sloth-selfie/src/Activities.js
@@ -70,7 +70,7 @@ function ActivitiesFunction(props){
                         {props.activities.filter(activity => !activity.completed).sort((a, b) => new Date(a.deadline) - new Date(b.deadline)).map(activity => (
                             <div className="activity-card" key={activity._id}>
                                 <h2>{activity.title}</h2>
-                                <p>Due: {activity.deadline}</p>
+                                <p>Due: {new Date(activity.deadline).toLocaleDateString()}</p>
                                 <button className="btn" onClick={() => handleRemoveActivity(activity._id, props.activities, props.setActivities)}>
                                     Done
                                 </button>
