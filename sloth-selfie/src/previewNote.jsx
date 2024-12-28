8d8a424277f2ad9716f04df7c7ac082b95ea1c5f
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2024-12-28 02:39:52 +0100

    fit stuff

--- sloth-selfie/src/previewNote.jsx
+++ sloth-selfie/src/previewNote.jsx
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import { initialNotes } from './Notes';
+import { fetchNotes } from "./NotesUtils";
 import noteImage from './media/note.png';
-import { marked } from 'marked';
+import NoteCard from './NoteCard';
 import './css/Notes.css';
 
 function PreviewNote() {
     const navigate = useNavigate();
-
-    const [isExpanded, setIsExpanded] = useState(false);
-    const longText = "Questo è un testo molto lungo che verrà troncato se supera un certo limite di caratteri. Quando clicchi sul pulsante 'Continua', potrai leggere il testo completo. Questa funzionalità è utile quando hai tanto contenuto da mostrare ma vuoi mantenere l'interfaccia pulita e ordinata.";
-
-    const toggleText = () => {
-        setIsExpanded(!isExpanded);
-    };
-
-    const limit = 100; // Limite di caratteri da visualizzare prima di troncare
-    const truncatedText = longText.slice(0, limit) + '...';
-
+    const [initialNotes, setInitialNotes] = useState([]);
 
     const handleLinkClick = (path) => (event) => {
         event.preventDefault();
@@ -28,73 +18,28 @@ function PreviewNote() {
         }, 300);
     };
 
-    
-    
+    useEffect(() => {
+        fetchNotes().then((notes) => {
+            setInitialNotes(notes);
+        });
+    }, []);
 
     return (
         <div className="inCard">
             <div className="notes-section">
                 <p>Your Notes:</p>
                 <div className="scrollable-list">
-                    {initialNotes.length > 0 ? (
-                        initialNotes.map((note, index) => (
-                            <div key={index} className="card-note mb-3"> {/* Bootstrap card */}
-                                <div className="card-body">
-                                    <h5>{note.title}</h5>
-                                    <small>Author: {note.author}</small><br />
-                                    <small>
-                                        Access: {note.access.type === 'public' 
-                                            ? 'Public' 
-                                            : note.access.type === 'private' 
-                                            ? 'Private' 
-                                            : `Shared with: ${note.access.allowedUsers.join(', ')}`}
-                                    </small>
-                                    {/* Render note content or todo list */}
-                                    {note.isTodo ? (
-                                        <div className="card-text">
-                                            <ul>
-                                            {/* Render the tasks if the note contains a todo list */}
-                                            {note.tasks && note.tasks.length > 0 ? (
-                                                note.tasks.map((task, taskIndex) => (
-                                                <li key={taskIndex}>
-                                                    <input
-                                                    type="checkbox"
-                                                    checked={task.completed}
-                                                    readOnly
-                                                    />
-                                                    {task.completed ? <s>{task.text}</s> : task.text}
-                                                    {task.deadline ? (
-                                                    <small>
-                                                        &nbsp; Deadline: {new Date(task.deadline).toLocaleDateString()}
-                                                    </small>
-                                                    ) : null}
-                                                </li>
-                                                ))
-                                            ) : (
-                                                <p>No tasks available</p>
-                                            )}
-                                            </ul>
-                                        </div>
-                                    ) : (
-                                        <div className="card-text">
-                                            {/* Limiting note content to 200 characters, showing markdown */}
-                                            <div
-                                                dangerouslySetInnerHTML={{
-                                                    __html: note.content.length > 200 
-                                                        ? marked(note.content.substring(0, 200) + "...")
-                                                        : marked(note.content)
-                                                }}
-                                            />
-                                        </div>
-                                    )}
-                                    {/* Using d-flex and justify-content-between for proper alignment */}
-                                    <div className="d-flex justify-content-between mt-3">
-                                        <small className="text-muted">Category: {note.category}</small><br />
-                                        <small className="text-muted">Created on: {new Date(note.createDate).toLocaleDateString()}</small>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
+                    {Array.isArray(initialNotes) && initialNotes.length > 0 ? (
+                        initialNotes.map((note) => {
+                            return (
+                                <NoteCard
+                                    key={note._id}
+                                    note={note}
+                                    setNotes={setInitialNotes}
+                                    isPreview={true}
+                                />
+                            );
+                        })
                     ) : (
                         <div className="div-postit">
                             <h2>No notes yet!</h2>
