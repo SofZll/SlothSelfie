c3309d7e099ffba7d9b982c648bfb6588995db36
giulia-t <giulia.tors@gmail.com>
2025-02-12 17:51:48 +0100

    added previewCalendar

--- sloth-selfie/src/cardCarosel.jsx
+++ sloth-selfie/src/cardCarosel.jsx
@@ -2,20 +2,16 @@ import './css/CarouselHome.css';
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
 import {Link, useNavigate} from 'react-router-dom';
-import Calendar from 'react-calendar';
 import './css/App.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'react-calendar/dist/Calendar.css';
 import PreviewPomodoro from './previewPomodoro';
 import PreviewNote from './previewNote';
-import { initialEvents } from './Calendar';
-import { fetchData } from './CalendarUtils';
+import PreviewCalendar from './previewCalendar';
 
 function Card({ title, caseShow }) {
     const [show, setShown] = useState(false);
     const navigate = useNavigate();
-    const [activities, setActivities] = useState([]);
-    const [event, setEvent] = useState([]);
 
     // animation page
     const handleLinkClick = (path) => (event) => {
@@ -34,45 +30,11 @@ function Card({ title, caseShow }) {
       : "0 2px 10px rgb(0 0 0 / 8%)"
     });
 
-    // Check if there is an event on the date
-    const hasEventOnDate = (date) => {
-        return event.some(event => event.date === date.toLocaleDateString('en-CA'));
-    };
-
-    // Check if there is an activity on the date
-    const hasActivityOnDate = (date) => {
-        return activities.some(activity => activity.deadline === date.toLocaleDateString('en-CA'));
-    };
-
-    // Adding a dot to the date if there is an event or an activity on that date
-    const tileContent = ({ date, view }) => {
-        if (view === 'month' && hasEventOnDate(date)) {
-            return <span className="event-indicator" style={{ backgroundColor: '#2b59b6', borderRadius: '50%', width: '10px', height: '10px', display: 'inline-block' }}></span>;
-        } else if (view === 'month' && hasActivityOnDate(date)) {
-            return <span className="event-indicator" style={{ backgroundColor: '#f72585', borderRadius: '50%', width: '10px', height: '10px', display: 'inline-block' }}></span>;
-        }
-    };
-
-    useEffect(() => {
-        fetchData('activities', setActivities);
-        fetchData('events', setEvent);
-    } , []);
-
-    
-
+   
     let content;
     switch (caseShow) {
         case "1":
-            content = (
-                <div className='inCard'>
-                    <Calendar  tileContent={tileContent}/>
-                    <div className="divBtn">
-                        <Link to="/calendar" onClick={handleLinkClick('/calendar')}>
-                            <button className="btn btn-main" >Manage Calendar</button>
-                        </Link>
-                    </div>
-                </div>
-            );
+            content = <PreviewCalendar />;
             break;
             case "2":
                 content = <PreviewNote />;
