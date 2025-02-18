5588186f683937679af59c669505dcce220bd3cd
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-02-18 21:45:00 +0100

    implemented email notification

--- backend/jobs/agenda.js
+++ backend/jobs/agenda.js
@@ -17,11 +17,10 @@ agenda.on('ready', async () => {
 agenda.define('send notification', async job => {
     console.log('Sending notification');
     console.log(job.attrs.data);
-    const { sender, receivers, message, type } = job.attrs.data;
+    const { notificationId } = job.attrs.data;
 
     try {
-        console.log(`Invio notifica ai destinatari: ${receivers}`);
-        if (emitNotification(sender, receivers, message, type)) {
+        if (emitNotification(notificationId)) {
             console.log('Notification sent successfully');
         } else {
             console.error('Error sending notification');
