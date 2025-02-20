dc127b9e1fdc27448ba20df6ca191ffc26ab6a01
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-02-20 19:16:44 +0100

    update chat box

--- sloth-selfie/src/MessageBox.js
+++ sloth-selfie/src/MessageBox.js
@@ -1,24 +1,177 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './css/MessageBox.css';
 import { useMediaQuery } from 'react-responsive';
+import Swal from 'sweetalert2';
 
-function MessageBox() {
+function MessageBox({ username}) {
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [newMessage, setNewMessage] = useState("");
     const [chat, setChat] = useState([
         {
-            username: 'User1',
+            author: {
+                username: 'User1',
+                image: 'https://via.placeholder.com/150'
+            },
+            messages: [
+                {
+                    author: 'User1',
+                    text: 'Hello1',
+                    date: '12:00'
+                },
+                {
+                    author: 'User2',
+                    text: 'Hi',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                },
+                {
+                    author: 'User2',
+                    text: 'Rispondimiiiiiiii',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                },
+                {
+                    author: 'User2',
+                    text: 'Hi',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                }
+            ],
             lastMessage: 'Hello'
         },
         {
-            username: 'User2',
+            author: {
+                username: 'User1',
+                image: 'https://via.placeholder.com/150'
+            },
+            messages: [
+                {
+                    author: 'User1',
+                    text: 'Hello2',
+                    date: '12:00'
+                },
+                {
+                    author: 'User2',
+                    text: 'Hi',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                }
+            ],
             lastMessage: 'Hi'
         },
         {
-            username: 'User3',
+            author: {
+                username: 'User1',
+                image: 'https://via.placeholder.com/150'
+            },
+            messages: [
+                {
+                    author: 'User1',
+                    text: 'Hello3',
+                    date: '12:00'
+                },
+                {
+                    author: 'User2',
+                    text: 'Hi',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                }
+            ],
+            lastMessage: 'Hey'
+        },
+        {
+            author: {
+                username: 'User1',
+                image: 'https://via.placeholder.com/150'
+            },
+            messages: [
+                {
+                    author: 'User1',
+                    text: 'Hello4',
+                    date: '12:00'
+                },
+                {
+                    author: 'User2',
+                    text: 'Hi',
+                    date: '12:01'
+                },
+                {   
+                    author: 'User2',
+                    text: 'Hey',
+                    date: '12:02'
+                }
+            ],
+            lastMessage: 'Hey'
+        },
+        {
+            author: {
+                username: 'User1',
+                image: 'https://via.placeholder.com/150'
+            },
+            messages: [],
             lastMessage: 'Hey'
         }
     ]);
+    const messagesEndRef = useRef(null);
     const isDesktop = useMediaQuery({ minWidth: 769 });
+
+    const handleChatClick = (chat) => {
+        setSelectedChat(chat);
+    }
+
+    const handleSendMessage = () => {
+        if (!newMessage.trim()) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Errore',
+                text: 'Inserisci un messaggio'
+            });
+        } else {
+            const updateChat = {
+                ...selectedChat,
+                messages: [
+                    ...selectedChat.messages,
+                    { 
+                        author: {username}, 
+                        text: newMessage, 
+                        date: new Date()
+                    }
+                ],
+                lastMessage: newMessage
+            };
+
+            setChat(chat.map(chat => chat === selectedChat ? updateChat : chat));
+            setSelectedChat(updateChat);
+            setNewMessage("");
+        }
+    }
+
+    useEffect(() => {
+        if (selectedChat) {
+            messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [selectedChat, selectedChat?.messages]);
 
     if (!isDesktop) return null;
 
@@ -28,17 +181,42 @@ function MessageBox() {
                 Messaggi
             </button>
             <div className={`message-box ${isOpen ? 'open' : ''}`}>
-                {chat.map((chat, index) => (
-                    <div key={index} className="chat">
-                        <div className="chat-container">
-                            <img src="https://via.placeholder.com/50" alt="profile" />
-                            <div className="chat-content">
-                                <p>{chat.username}</p>
-                                <p>{chat.lastMessage}</p>
-                            </div>
+                {selectedChat ? (
+                    <div className="chat-selected">
+                        <div className="chat-selected-header">
+                            <span onClick={() => handleChatClick(null)}>Back</span>
+                            <img src={selectedChat.author.image} alt="profile" />
+                            <h6>{selectedChat.author.username}</h6>
+                        </div>
+                        <div className="chat-selected-messages">
+                            {selectedChat.messages.map((message, index) => (
+                                <div key={index} className={`message ${message.author === 'User1' ? 'sent' : 'received'}`}>
+                                    <p>{message.text}</p>
+                                    <span>{message.date}</span>
+                                </div>
+                            ))}
+                            <div ref={messagesEndRef}></div>
+                        </div>
+                        <div className="chat-input">
+                            <input type="text" placeholder="Scrivi un messaggio" />
+                            <button onClick={handleSendMessage}>Invia</button>
                         </div>
                     </div>
-                ))}
+                ) : (
+                    <div className="chat-all">
+                        {chat.map((chat, index) => (
+                            <div key={index} className="chat" onClick={() => handleChatClick(chat)}>
+                                <div className="chat-container">
+                                    <img src={chat.author.image} alt="profile" />
+                                    <div className="chat-content">
+                                        <span>{chat.author.username}</span>
+                                        <p>{chat.lastMessage}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
