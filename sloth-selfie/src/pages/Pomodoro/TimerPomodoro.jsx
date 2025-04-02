8a0fe680bb425837171e684597e2b19626ce0e7f
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2025-04-02 10:05:44 +0200

    animazione

--- sloth-selfie/src/pages/Pomodoro/TimerPomodoro.jsx
+++ sloth-selfie/src/pages/Pomodoro/TimerPomodoro.jsx
@@ -1,17 +1,16 @@
 import React, {useState, useEffect} from 'react';
 
 import { usePomodoro } from '../../contexts/PomodoroContext';
+import { AnimationPencil } from './AnimationPencil';
 
-import { Share, Pen, RotateCcw, SkipForward, CirclePlay, CirclePause, CircleStop, CirclePlus } from 'lucide-react';
-import { use } from 'react';
+
+import { Share, Pen, RotateCcw, SkipForward, SkipBack, CirclePlay, CirclePause, CircleStop, CirclePlus, CalendarPlus, Music } from 'lucide-react';
+
 
 
 const TimerPomodoro = () => {
-    const { pomodoro, setPomodoro, settingsPomodoro, setSettingsPomodoro, increasePomodoroTime, addCycle, resetPomodoro, newPomodoro, editSettingsPomodoro, skipTime } = usePomodoro();
-    const [play, setPlay] = useState(false);
+    const { play, setPlay, pomodoro, settingsPomodoro, increasePomodoroTime, addCycle, resetPomodoro, newPomodoro, skipTime, skipBack, popUp, setPopUp } = usePomodoro();
     const [inShare, setInShare] = useState(false);
-    const [popupShare, SetPopupShare] = useState(false);
-    const [popupEdit, SetPopupEdit] = useState(false);
 
     const formatTime = (time) => {
         const minutes = Math.floor(time / 60);
@@ -23,6 +22,9 @@ const TimerPomodoro = () => {
         if (pomodoro.finished) setPlay(false);
     }, [pomodoro.finished]);
             
+    useEffect(() => {
+        setPlay(false);
+    }, [pomodoro.isStudyTime]);
 
     useEffect(() => {
         if (!inShare) {
@@ -39,102 +41,97 @@ const TimerPomodoro = () => {
 
     return (
         <div>
-            <div className='row w-100'>
-                <div className='col'>
-                    <button className='btn' onClick={() => SetPopupShare(true)}>
-                        <Share size='30' color='#244476' strokeWidth='1.5' />
-                    </button>
-                </div>
+            <div className='d-flex justify-content-between'>
+                <button className='btn' onClick={() => setPopUp({ ...popUp, share: !popUp.share })}>
+                    <Share size='30' color='#244476' strokeWidth='1.5' />
+                </button>
+
+                <button className='btn' onClick={() => setPopUp({ ...popUp, calendar: !popUp.calendar })}>
+                    <CalendarPlus size='30' color='#244476' strokeWidth='1.5' />
+                </button>
+                
+                <button className='btn' onClick={() => setPopUp({ ...popUp, music: !popUp.music })}>
+                    <Music size='30' color='#244476' strokeWidth='1.5' />
+                </button>
             </div>
 
-            <div className='row w-100'>
-                <div className='col'>
-                    {pomodoro.finished ? (
-                        <>
+            <div className='d-flex justify-content-center'>
+                {pomodoro.finished ? (
+                    <>
                         Congratulation! <br /> You have studied for {formatTime(pomodoro.studiedTime)} minutes
-                        </>
-                    ) : (
+                    </>
+                ) : (
+                    <>
+                    {pomodoro.started ? (
                         <>
-                        {pomodoro.started ? (
-                            <>
                             You still have {formatTime(pomodoro.timeLeft)} minutes {pomodoro.isStudyTime ? 'to study' : 'in your break'}
-                            </>
-                        ) : (
-                            <>
-                            Let's start a new Pomodoro session
-                            </>
-                        )}
-                        </>
-                    )}
-                </div>
-            </div>
-
-            <div className='row w-100'>
-                <div className='col'>
-                    {/* TODO: animazione */}
-                </div>
-            </div>
-
-            <div className='row w-100'>
-                <div className='col'>
-                    {pomodoro.finished ? (
-                        <>
-                        Reset or add a cycle to your Pomodoro session
                         </>
                     ) : (
                         <>
-                        {pomodoro.started ? (
-                            <>
-                            Cycle {settingsPomodoro.cycles + settingsPomodoro.additionalCycles - pomodoro.cyclesLeft + 1} of {settingsPomodoro.cycles + settingsPomodoro.additionalCycles}
-                            </>
-                        ) : (
-                            <>
-                            Press play to start the Pomodoro session
-                            </>
-                        )}
+                            Let's start a new Pomodoro session
                         </>
                     )}
-                </div>
+                    </>
+                )}
             </div>
 
-            <div className='row w-100'>
-                <div className='col'>
-                    <button className='btn' onClick={() => SetPopupEdit(true)}>
-                        <Pen size='30' color='#244476' strokeWidth='1.5' />
-                    </button>
-                </div>
-                <div className='col'>
+            <div className='d-flex justify-content-center'>
+                <AnimationPencil />
+            </div>
+
+            <div className='d-flex justify-content-center'>
+                {pomodoro.finished ? (
+                    <>
+                        Reset or add a cycle to your Pomodoro session
+                    </>
+                ) : (
+                    <>
+                    {pomodoro.started ? (
+                        <>
+                            Cycle {settingsPomodoro.cycles + settingsPomodoro.additionalCycles - pomodoro.cyclesLeft + 1} of {settingsPomodoro.cycles + settingsPomodoro.additionalCycles}
+                        </>
+                    ) : (
+                        <>
+                            Press play to start the Pomodoro session
+                        </>
+                    )}
+                    </>
+                )}
+            </div>
+
+            <div className='d-flex justify-content-between'>
+                <button className='btn' onClick={() => setPopUp({ ...popUp, edit: !popUp.edit })}>
+                    <Pen size='30' color='#244476' strokeWidth='1.5' />
+                </button>
+
+                {pomodoro.finished ? (
                     <button className='btn' onClick={() => resetPomodoro()}>
                         <RotateCcw size='30' color='#244476' strokeWidth='1.5' />
                     </button>
-                </div>
-
-                {pomodoro.finished ? (
-                    <div className='col'>
-                        <button className='btn' onClick={() => newPomodoro()}>
-                            <CircleStop size='30' color='#244476' strokeWidth='1.5' />
-                        </button>
-                    </div>
                 ) : (
-                    <div className='col'>
-                        <button className='btn' onClick={() => setPlay(!play)}>
-                            {play ? <CirclePause size='30' color='#244476' strokeWidth='1.5' /> : <CirclePlay size='30' color='#244476' strokeWidth='1.5' />}
-                        </button>
-                    </div>
+                    <button className='btn' onClick={() => skipBack()}>
+                        <SkipBack size='30' color='#244476' strokeWidth='1.5' />
+                    </button>
                 )}
 
                 {pomodoro.finished ? (
-                    <div className='col'>
-                        <button className='btn' onClick={() => addCycle()}>
-                            <CirclePlus size='30' color='#244476' strokeWidth='1.5' />
-                        </button>
-                    </div>
+                    <button className='btn' onClick={() => newPomodoro()}>
+                        <CircleStop size='30' color='#244476' strokeWidth='1.5' />
+                    </button>
                 ) : (
-                    <div className='col'>
-                        <button className='btn' onClick={() => skipTime()}>
-                            <SkipForward size='30' color='#244476' strokeWidth='1.5' />
-                        </button>
-                    </div>
+                    <button className='btn' onClick={() => setPlay(!play)}>
+                        {play ? <CirclePause size='30' color='#244476' strokeWidth='1.5' /> : <CirclePlay size='30' color='#244476' strokeWidth='1.5' />}
+                    </button>
+                )}
+
+                {pomodoro.finished ? (
+                    <button className='btn' onClick={() => addCycle()}>
+                        <CirclePlus size='30' color='#244476' strokeWidth='1.5' />
+                    </button>
+                ) : (
+                    <button className='btn' onClick={() => skipTime()}>
+                        <SkipForward size='30' color='#244476' strokeWidth='1.5' />
+                    </button>
                 )}
 
             </div>
