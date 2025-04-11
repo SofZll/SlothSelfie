66071f71fcba7e53057152f101a067c044d56987
giulia-t <giulia.tors@gmail.com>
2025-04-11 16:42:51 +0200

    Projects: gantt fix

--- sloth-selfie/src/components/GanttChartView.jsx
+++ sloth-selfie/src/components/GanttChartView.jsx
@@ -23,26 +23,68 @@ const GanttChartView = ({ projectId }) => {
     fetchProject();
   }, [projectId]);
 
+  //function to adjust the activities with 0 days duration for gantt
+  const AdjustZeroDuration = (activity) => {
+    const start = new Date(activity.startDate);
+    let end = new Date(activity.deadline);
+    
+    if (end <= start) {
+      end.setHours(start.getHours() + 1); // Adds 1 hour
+    }
+  
+    return { ...activity, deadline: end };
+  };
+
+  const disableDefaultBehaviour = (gantt) => {
+    // Disable the default gantt behavior
+    gantt.bind_bar = () => {};
+    gantt.bind_bar_progress = () => {};
+    gantt.bind_resize = () => {};
+    gantt.bind_dependency = () => {};
+    gantt.update_bar_position = () => {};
+    gantt.setup_dependencies = () => {};
+    gantt.unselect_all = () => {};
+
+    // Overwrite the SVG listeners to disable the default behavior
+    gantt.$svg.addEventListener('mousedown', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    }, true);
+
+    gantt.$svg.addEventListener('mouseup', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    }, true);
+
+    gantt.$svg.addEventListener('mousemove', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    }, true);
+  }
+
   useEffect(() => {
     if (project) {
       // Creates activities for the Gantt chart
       const tasks = [];
       project.phases.forEach(phase => {
+        // Adjust zero duration activities
+        const adjustedMacroActivity = AdjustZeroDuration(phase.macroActivity);
         // Adds macro of the phase
         tasks.push({
           id: `macro-${phase.macroActivity._id}`,
           name: `Macro: ${phase.macroActivity.title}`,
           start: new Date(phase.macroActivity.startDate),
-          end: new Date(phase.macroActivity.deadline),
+          end: new Date(adjustedMacroActivity.deadline),  
         });
 
         // Adds activities of the phase macro
         phase.activities.forEach(activity => {
+        const adjustedActivity = AdjustZeroDuration(activity);
           tasks.push({
             id: activity._id,
             name: activity.title,
             start: new Date(activity.startDate),
-            end: new Date(activity.deadline),
+            end: new Date(adjustedActivity.deadline),
             assignee: activity.sharedWith.map(a => a.username).join(", "),
             dependencies: activity.dependencies || [],
             custom_class: `activities-of-${phase.macroActivity._id}`,
@@ -50,21 +92,23 @@ const GanttChartView = ({ projectId }) => {
         });
 
         phase.subphases.forEach(subphase => {
+        const adjustedSubphaseMacroActivity = AdjustZeroDuration(subphase.macroActivity);
         // Adds macro of the subphase
           tasks.push({
             id: `macro-${subphase.macroActivity._id}`,
             name: `Macro: ${subphase.macroActivity.title}`,
             start: new Date(subphase.macroActivity.startDate),
-            end: new Date(subphase.macroActivity.deadline),
+            end: new Date(adjustedSubphaseMacroActivity.deadline),
           });
 
           //adds activities of the subphase macro
           subphase.activities.forEach(activity => {
+            const adjustedActivity = AdjustZeroDuration(activity);
             tasks.push({
               id: activity._id,
               name: activity.title,
               start: new Date(activity.startDate),
-              end: new Date(activity.deadline),
+              end: new Date(adjustedActivity.deadline),
               assignee: activity.sharedWith.map(a => a.username).join(", "),
               dependencies: activity.dependencies || [],
               custom_class: `activities-of-${subphase.macroActivity._id}`,
@@ -83,6 +127,9 @@ const GanttChartView = ({ projectId }) => {
             if (!task) return;
           },
         });
+
+        //Disable default gantt behavior
+        disableDefaultBehaviour(gantt);
         gantt.render();
       }
     }
@@ -93,7 +140,7 @@ const GanttChartView = ({ projectId }) => {
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <div id="gantt-container" style={{ flex: 1, height: "500px" }}></div>
+        <div id="gantt-container" style={{ flex: 1, height: "300px" }}></div>
       </div>
     </div>
   );
