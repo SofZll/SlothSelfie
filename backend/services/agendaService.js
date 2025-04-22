fcb3629cc97e3f7cf752f7a2c0d8c061c8c00c40
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-04-22 15:22:40 +0200

    notification repeat working agenda and notifications.jsx

--- backend/services/agendaService.js
+++ backend/services/agendaService.js
@@ -1,14 +1,28 @@
-import agenda from '../agenda/agenda';
+const agenda = require('../agenda/agenda');
+const User = require('../models/userModel');
 
 const getScheduledJobs = async (userId, hours = 24) => {
     const now = new Date(); // da cambiare con TM
     const limit = new Date(now.getTime() + hours * 60 * 60 * 1000);
 
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    console.log('User:', user);
+    console.log('User ID:', user._id);
+    console.log('User ID:', userId);
+    console.log('Now:', now);
+    console.log('Limit:', limit);
+
     const jobs = await agenda.jobs({ 
         name: 'send-notification',
         nextRunAt: { $gte: now, $lte: limit },
-        'data.userId': userId 
+        'data.notification.user': user._id
     });
+
+    console.log('Jobs:', jobs);
 
     return jobs;
 }
@@ -19,9 +33,13 @@ const formatJob = (job) => {
     return {
         _id: job.attrs._id,
         notificationId: notification._id,
-        title: notification.title,
+        title: notification.element.title,
         type: notification.type,
+        elementType: notification.elementType,
+        mode: notification.mode,
         variant: notification.variant,
+        urgency: notification.urgency,
+        snooze: notification.snooze,
         triggerAt: job.attrs.nextRunAt,
         to: new Date(notification.to),
         status: job.attrs.lastFinishedAt ? 'inactive' : 'active',
