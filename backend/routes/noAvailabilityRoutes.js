a7b80647e52fafa1e9f3f1eeb3507680a23cf066
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2025-04-24 18:35:03 +0200

    fix edit noAvailability

--- backend/routes/noAvailabilityRoutes.js
+++ backend/routes/noAvailabilityRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const { addNoAvailability, getNoAvailability, deleteNoAvailability } = require('../controllers/noAvailabilityController');
+const { addNoAvailability, getNoAvailability, deleteNoAvailability, updateNoAvailability } = require('../controllers/noAvailabilityController');
 const router = express.Router();
 
 // No availability endpoints
 router.get('/no-availabilities', getNoAvailability);
 router.post('/no-availability', addNoAvailability);
-// router.put('/no-availability/:noAvailabilityId', updateNoAvailability);
+router.put('/no-availability/:noAvailabilityId', updateNoAvailability);
 router.delete('/no-availability/:noAvailabilityId', deleteNoAvailability);
 
 module.exports = router;
