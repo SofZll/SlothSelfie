b66f588064ec9d21c76e7656d54c26b12f6c06eb
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2025-04-28 15:21:20 +0200

    accepted activity projects

--- sloth-selfie/src/components/ScrollList.jsx
+++ sloth-selfie/src/components/ScrollList.jsx
@@ -6,7 +6,7 @@ const ScrollList = ({ CardList, smallView }) => {
     return (
         <>
             {CardList.length > 0 && (
-                <div className='d-flex flex-column mx-md-0 m-2 overflow-y-auto overflow-x-hidden bg-white border rounded w-100'>
+                <div className='d-flex flex-column mx-md-0 m-2 overflow-y-auto overflow-x-hidden bg-white border rounded w-100' style={{ maxHeight: '30vh' }}>
                     {CardList.map((card, index) => (
                         <div key={index} className='d-flex flex-column w-100 p-md-3 p-2'>
                             <CardActivity Activity={card} smallView={smallView} />
