6d586f2b46ed3ce3788a19488a238164b308a679
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-05-14 13:46:18 +0200

    versione funzionante

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const multer = require('multer');// Import multer for file upload handling
-const upload = multer({ dest: 'uploads/' }); // temporary storage for uploaded .ics files
+const path = require('path');
+//const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 const { createNewEvent, getEvents, updateEvent, deleteEvent, exportEvent, importEvents } = require('../controllers/eventController');
 
@@ -10,7 +11,7 @@ const router = express.Router();
 // Event endpoints
 router.get('/events', getEvents);
 router.post('/event', createNewEvent);
-router.post('/events/import', upload.array('icsFiles'), importEvents);  
+//router.post('/events/import', upload.array('icsFiles'), importEvents);  
 router.put('/event/:eventId', updateEvent);
 router.delete('/event/:eventId', deleteEvent);
 router.get('/event/:eventId/export', exportEvent);
