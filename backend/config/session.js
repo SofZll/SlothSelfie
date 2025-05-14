323e12707f4f4ead2a4247c17e2de5f463c26c08
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-05-14 13:46:18 +0200

    versione funzionante

--- backend/config/session.js
+++ backend/config/session.js
@@ -2,11 +2,29 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 require('dotenv').config();
 
+const mongoCredentials = {
+    user: "site232453",
+    pwd: "au8Eevai",
+    site: "mongo_site232453"
+};
+
+/*
+const mongoCredentials = {
+    user: "kaorijiang",
+    pwd: "dWFd3wNQCJQksWEs",
+    site: "cluster0.ynn63.mongodb.net",
+    dbname: "slothselfie"
+};
+*/
+
+const dbName = 'slothselfie';
+const uri = `mongodb://${mongoCredentials.user}:${mongoCredentials.pwd}@${mongoCredentials.site}/${dbName}?authSource=admin&writeConcern=majority`;
+
 module.exports = session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || 'defaultsecret',
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    store: MongoStore.create({ mongoUrl: uri }),
     cookie: {
         secure: false, 
         httpOnly: true,
