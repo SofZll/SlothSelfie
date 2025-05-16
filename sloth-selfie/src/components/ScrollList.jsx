a43f7c89a6d1666d4772ab9c21ac00f28ab0eaca
Sofy_zan <sofia.zanelli3@studio.unibo.it>
2025-05-16 16:04:56 +0200

    zoom 200 parte 2

--- sloth-selfie/src/components/ScrollList.jsx
+++ sloth-selfie/src/components/ScrollList.jsx
@@ -7,7 +7,7 @@ const ScrollList = ({ CardList, smallView, activity }) => {
     return (
         <>
             {CardList.length > 0 && (
-                <div className='d-flex flex-column mx-md-0 m-2 overflow-y-auto overflow-x-hidden bg-white border rounded w-100' style={{ maxHeight: '30vmin' }}>
+                <div className='d-flex flex-column mx-md-0 m-2 overflow-y-auto overflow-x-hidden bg-white border rounded w-100' style={{ maxHeight: '35vmin' }}>
                     {CardList.map((card, index) => (
                         <div key={index} className='d-flex flex-column w-100 p-md-3 p-2'>
                             {activity ? (
