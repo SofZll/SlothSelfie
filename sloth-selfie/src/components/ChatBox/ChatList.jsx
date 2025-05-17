54247c9c07c66e39b88ccd1d79017a74270b1de1
Kmoon7 <kaori.jiang@studio.unibo.it>
2025-05-17 12:17:03 +0200

    una riga letteralmente

--- sloth-selfie/src/components/ChatBox/ChatList.jsx
+++ sloth-selfie/src/components/ChatBox/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo , useEffect, useContext} from 'react';
+import React, { useMemo , useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useChat } from '../../contexts/ChatContext';
